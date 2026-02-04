a376d8fd6d0755bb7fe9638c832caed22a563c95
“floseptysia-hue” <floseptysia@gmail.com>
2026-02-04 22:27:23 +0700

    handle ci/cd

--- tests/test_higo.py
+++ tests/test_higo.py
@@ -8,7 +8,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def setup_driver():
     chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--window-size=1280,720")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+
     return webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
         options=chrome_options
